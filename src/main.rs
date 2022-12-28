5cc17a798364296ed0d284cc916d50c4597b85d0
Saref111 <saref012@gmail.com>
2022-12-28 11:51:00 +0200

    split url

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_owned());
+    let address = String::from("127.0.0.1:8080");
+
+    let server = Server::new(address);
     server.run();
 }
 
 struct Server {
     address: String,
+    path: String,
+    port: String,
 }
 
 impl Server {
     fn new(address: String) -> Self {
-        Self { address }
+        let url: Vec<&str> = address.split(':').collect();
+        let mut path: String = String::from("localhost");
+        let mut port: String = String::from("8888");
+
+        for (i, &a) in url.iter().enumerate() {
+            match i {
+                0 => path = a.to_owned(),
+                1 => port = a.to_owned(),
+                _ => break,
+            }
+        }
+        Self {
+            address,
+            path,
+            port,
+        }
     }
 
     fn run(&self) {
