e7bdb1bd0c44848a0afa3b43b74fd8cda0c180e7
Saref111 <saref012@gmail.com>
2022-12-28 12:25:43 +0200

    implemented run mthd

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,28 @@ struct Server {
 
 impl Server {
     fn new(address: String) -> Self {
-        // let url: Vec<&str> = address.split(':').collect();
+        let url: Vec<&str> = address.split(':').collect();
 
-        let semicolons_idx = match address.find(':') {
-            Some(val) => val,
-            None => 0,
-        };
+        // let semicolons_idx = match address.find(':') {
+        //     Some(val) => val,
+        //     None => 0,
+        // };
 
         let mut host: String = String::from("localhost");
         let mut port: String = String::from("8888");
 
-        if semicolons_idx > 0 {
-            host = address[..semicolons_idx].to_owned();
-            port = address[semicolons_idx + 1..].to_owned();
-        }
-
-        // for (i, &a) in url.iter().enumerate() {
-        //     match i {
-        //         0 => host = a.to_owned(),
-        //         1 => port = a.to_owned(),
-        //         _ => break,
-        //     }
+        // if semicolons_idx > 0 {
+        //     host = address[..semicolons_idx].to_owned();
+        //     port = address[semicolons_idx + 1..].to_owned();
         // }
+
+        for (i, &a) in url.iter().enumerate() {
+            match i {
+                0 => host = a.to_owned(),
+                1 => port = a.to_owned(),
+                _ => break,
+            }
+        }
 
         Self {
             address,
@@ -44,6 +44,6 @@ impl Server {
     }
 
     fn run(&self) {
-        todo!()
+        println!("Server is listening on {}:{}", self.host, self.port)
     }
 }
