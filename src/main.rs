b0477433320f5ea313d000f8c74c7929ac2a9f33
Saref111 <saref012@gmail.com>
2022-12-28 12:02:42 +0200

    split strinf second implementation

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,38 @@ fn main() {
 
 struct Server {
     address: String,
-    path: String,
+    host: String,
     port: String,
 }
 
 impl Server {
     fn new(address: String) -> Self {
-        let url: Vec<&str> = address.split(':').collect();
-        let mut path: String = String::from("localhost");
+        // let url: Vec<&str> = address.split(':').collect();
+
+        let semicolons_idx = match address.find(':') {
+            Some(val) => val,
+            None => 0,
+        };
+
+        let mut host: String = String::from("localhost");
         let mut port: String = String::from("8888");
 
-        for (i, &a) in url.iter().enumerate() {
-            match i {
-                0 => path = a.to_owned(),
-                1 => port = a.to_owned(),
-                _ => break,
-            }
+        if semicolons_idx > 0 {
+            host = address[..semicolons_idx].to_owned();
+            port = address[semicolons_idx + 1..].to_owned();
         }
+
+        // for (i, &a) in url.iter().enumerate() {
+        //     match i {
+        //         0 => host = a.to_owned(),
+        //         1 => port = a.to_owned(),
+        //         _ => break,
+        //     }
+        // }
+
         Self {
             address,
-            path,
+            host,
             port,
         }
     }
