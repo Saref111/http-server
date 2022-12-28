f84380fcccd5f3b58f95e2c2595aeac938f2cefb
Saref111 <saref012@gmail.com>
2022-12-28 10:51:15 +0200

    added interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_owned());
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Self { address }
+    }
+
+    fn run(&self) {
+        todo!()
+    }
 }
