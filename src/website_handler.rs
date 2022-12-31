5ca980fa3571de49d1a175fe62a1dd0a1b6c36f9
Saref111 <saref012@gmail.com>
2022-12-31 17:06:12 +0200

    added public path

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,32 @@
 use super::server::Handler;
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Method, Request, Response, StatusCode};
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>It woks !!!</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Bip</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            Method::POST => todo!(),
+            Method::DELETE => todo!(),
+            Method::PUT => todo!(),
+            Method::HEAD => todo!(),
+            Method::CONNECT => todo!(),
+            Method::TRACE => todo!(),
+            Method::PATCH => todo!(),
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
