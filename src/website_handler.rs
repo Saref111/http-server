0f7315d893ee70668351a0f416a0f51448d296b4
Saref111 <saref012@gmail.com>
2023-01-05 08:43:32 +0200

    added publick path check

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use super::server::Handler;
 use crate::http::{Method, Request, Response, StatusCode};
 
@@ -9,15 +11,34 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(p) => {
+                if p.starts_with(&self.public_path) {
+                    fs::read_to_string(p).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Bip</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    some => Response::new(StatusCode::Ok, some),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             Method::POST => todo!(),
             Method::DELETE => todo!(),
